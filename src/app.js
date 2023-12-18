78f090d77233c39cd3aaa83b11d96a40a550d8fa
Gabriel Delgado <6327@holbertonstudents.com>
2023-12-18 17:10:58 +0000

    Keeping the data local

--- src/app.js
+++ src/app.js
@@ -1,34 +1,17 @@
+const newData = require('./getDataFromDB')
 const express = require('express')
-const { createClient } = require('@supabase/supabase-js')
-
 // Create a single supabase client for interacting with your database
-const supabase = createClient('https://ireumvmoybitxftsqlgh.supabase.co/', process.env.SUPABASE_KEY)
 const app = express()
 app.get('/', async (req, res) => {
   try {
-    // Fetch data from Supabase
-    const { data, error } = await supabase.from('sakuga').select()
-
-    if (error) {
-      console.error(error)
-      res.status(500).send('Internal Server Error')
-      return
-    }
-
-    // Check if there is any data
-    if (!data || data.length === 0) {
-      res.status(404).send('No data found')
-      return
-    }
-
     // Choose a random index
-    const randomIndex = Math.floor(Math.random() * data.length)
-    console.log('The random KA you got was made by', data[randomIndex].author)
+    const randomIndex = Math.floor(Math.random() * newData.length)
+    console.log('The random KA you got was made by', newData[randomIndex].author)
     // Set the Content-Type header to indicate that you are sending an image
     res.setHeader('Content-Type', 'application/json')
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
     // Send the image file
-    res.send(data[randomIndex])
+    res.send(newData[randomIndex])
   } catch (err) {
     console.error('Error:', err.message)
     res.status(500).send('Internal Server Error')
---
+++ src/getDataFromDB.js
@@ -0,0 +1,53 @@
+const data =
+[
+  {
+    id: 2,
+    created_at: '2023-12-15T20:03:22.583816+00:00',
+    data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/lupin-the-third?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL2x1cGluLXRoZS10aGlyZCIsImlhdCI6MTcwMjY3MDUzMiwiZXhwIjoxNzAzMjc1MzMyfQ.HtIO47A5_qbcSxumSWYHodfrhFqW96cZ55YVQy7_KvQ&t=2023-12-15T20%3A00%3A24.716Z',
+    author: 'Takeshi Koike',
+    series: 'Lupin the III',
+    source: "Lupin the IIIrd: Goemon's Blood Spray (movie)"
+  },
+  {
+    id: 3,
+    created_at: '2023-12-15T20:03:54.505655+00:00',
+    data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/jjk-pen?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL2pqay1wZW4iLCJpYXQiOjE3MDI2NzA2MDksImV4cCI6MTcwMzI3NTQwOX0.5VuHwXVcswBQY2MXUDn8tQGbv71gTuHAhUKPoNhpCDc&t=2023-12-15T20%3A01%3A42.396Z',
+    author: 'Daniel Kim',
+    series: 'Jujutsu Kaisen',
+    source: 'Season 2: Episode #5'
+  },
+  {
+    id: 1,
+    created_at: '2023-12-15T20:01:47.329445+00:00',
+    data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/one-piece-g5?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL29uZS1waWVjZS1nNSIsImlhdCI6MTcwMjY3MDM2MSwiZXhwIjoxNzAzMjc1MTYxfQ.HO51R29_xhu_ZBKsG-Stj00BxEZhEtFLq2CyPLL8EPA&t=2023-12-15T19%3A57%3A33.907Z',
+    author: 'Shinya Ohira',
+    series: 'One Piece',
+    source: 'Episode #1072'
+  },
+  {
+    id: 4,
+    created_at: '2023-12-15T20:04:26.931945+00:00',
+    data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/one-piece-law?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL29uZS1waWVjZS1sYXciLCJpYXQiOjE3MDI5MDYyOTgsImV4cCI6MTcwNTQ5ODI5OH0.eoX2C-WeRUO_oWgl71ZK04ez3tdYnKHWRAqJ7PY7-9E&t=2023-12-18T13%3A29%3A50.179Z',
+    author: 'Masami Mori',
+    series: 'One Piece',
+    source: 'WE ARE ONE (special)'
+  },
+  {
+    id: 5,
+    created_at: '2023-12-18T14:02:46.290153+00:00',
+    data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/Sou-miyazaki_jjk?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL1NvdS1taXlhemFraV9qamsiLCJpYXQiOjE3MDI5MDgxNDAsImV4cCI6MTcwNTUwMDE0MH0.nGLZJ_rZsRvZsqNRgys8iDi1EDwIj2cZYnB6E9MHZyY&t=2023-12-18T14%3A00%3A31.581Z',
+    author: 'Sou Miyazaki',
+    series: 'Jujutsu Kaisen',
+    source: 'Season 2: Episode #1\n'
+  },
+  {
+    id: 6,
+    created_at: '2023-12-18T14:11:57.998632+00:00',
+    data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/vincent-chansard_jjk?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL3ZpbmNlbnQtY2hhbnNhcmRfamprIiwiaWF0IjoxNzAyOTA4NTY5LCJleHAiOjE3MDU1MDA1Njl9.b0s_-6THln7iVvqAhhDv1q2GNX-wnPYdIjQt4nqPEJE&t=2023-12-18T14%3A07%3A40.348Z',
+    author: 'Vincent Chansard',
+    series: 'Jujutsu Kaisen',
+    source: 'Season 2: Episode #17'
+  }
+]
+
+module.exports = data
