d5f6781f1d256ec84ea2f214f9f1eff8c5926024
Gabriel Delgado <6327@holbertonstudents.com>
2023-12-18 20:09:25 +0000

    Letp

--- src/getDataFromDB.js
+++ src/getDataFromDB.js
@@ -1,30 +1,6 @@
 const data =
 [
   {
-    id: 2,
-    created_at: '2023-12-15T20:03:22.583816+00:00',
-    data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/lupin-the-third?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL2x1cGluLXRoZS10aGlyZCIsImlhdCI6MTcwMjY3MDUzMiwiZXhwIjoxNzAzMjc1MzMyfQ.HtIO47A5_qbcSxumSWYHodfrhFqW96cZ55YVQy7_KvQ&t=2023-12-15T20%3A00%3A24.716Z',
-    author: 'Takeshi Koike',
-    series: 'Lupin the III',
-    source: "Lupin the IIIrd: Goemon's Blood Spray (movie)"
-  },
-  {
-    id: 3,
-    created_at: '2023-12-15T20:03:54.505655+00:00',
-    data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/jjk-pen?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL2pqay1wZW4iLCJpYXQiOjE3MDI2NzA2MDksImV4cCI6MTcwMzI3NTQwOX0.5VuHwXVcswBQY2MXUDn8tQGbv71gTuHAhUKPoNhpCDc&t=2023-12-15T20%3A01%3A42.396Z',
-    author: 'Daniel Kim',
-    series: 'Jujutsu Kaisen',
-    source: 'Season 2: Episode #5'
-  },
-  {
-    id: 1,
-    created_at: '2023-12-15T20:01:47.329445+00:00',
-    data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/one-piece-g5?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL29uZS1waWVjZS1nNSIsImlhdCI6MTcwMjY3MDM2MSwiZXhwIjoxNzAzMjc1MTYxfQ.HO51R29_xhu_ZBKsG-Stj00BxEZhEtFLq2CyPLL8EPA&t=2023-12-15T19%3A57%3A33.907Z',
-    author: 'Shinya Ohira',
-    series: 'One Piece',
-    source: 'Episode #1072'
-  },
-  {
     id: 4,
     created_at: '2023-12-15T20:04:26.931945+00:00',
     data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/one-piece-law?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL29uZS1waWVjZS1sYXciLCJpYXQiOjE3MDI5MDYyOTgsImV4cCI6MTcwNTQ5ODI5OH0.eoX2C-WeRUO_oWgl71ZK04ez3tdYnKHWRAqJ7PY7-9E&t=2023-12-18T13%3A29%3A50.179Z',
@@ -33,21 +9,36 @@ const data =
     source: 'WE ARE ONE (special)'
   },
   {
+    id: 6,
+    created_at: '2023-12-18T14:11:57.998632+00:00',
+    data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/vincent_chansard_jjks2ep17.gif?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL3ZpbmNlbnRfY2hhbnNhcmRfamprczJlcDE3LmdpZiIsImlhdCI6MTcwMjkyOTQ3MywiZXhwIjoxNzA1NTIxNDczfQ._5eoa3CaX853g7w5yZtyAEsLOdW6IDfZK4smsmrfoxM&t=2023-12-18T19%3A56%3A06.818Z',
+    author: 'Vincent Chansard',
+    series: 'Jujutsu Kaisen',
+    source: 'Season 2: Episode #17'
+  },
+  {
     id: 5,
     created_at: '2023-12-18T14:02:46.290153+00:00',
-    data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/Sou-miyazaki_jjk?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL1NvdS1taXlhemFraV9qamsiLCJpYXQiOjE3MDI5MDgxNDAsImV4cCI6MTcwNTUwMDE0MH0.nGLZJ_rZsRvZsqNRgys8iDi1EDwIj2cZYnB6E9MHZyY&t=2023-12-18T14%3A00%3A31.581Z',
+    data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/sou_miyazaki_jjks2ep1.gif?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL3NvdV9taXlhemFraV9qamtzMmVwMS5naWYiLCJpYXQiOjE3MDI5Mjk1MDgsImV4cCI6MTcwNTUyMTUwOH0.NKmbbw8XZn6k48J22ue7ROp1X0B3jbHYikhn7h3gBvs&t=2023-12-18T19%3A56%3A45.641Z',
     author: 'Sou Miyazaki',
     series: 'Jujutsu Kaisen',
     source: 'Season 2: Episode #1\n'
   },
   {
-    id: 6,
-    created_at: '2023-12-18T14:11:57.998632+00:00',
-    data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/vincent-chansard_jjk?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL3ZpbmNlbnQtY2hhbnNhcmRfamprIiwiaWF0IjoxNzAyOTA4NTY5LCJleHAiOjE3MDU1MDA1Njl9.b0s_-6THln7iVvqAhhDv1q2GNX-wnPYdIjQt4nqPEJE&t=2023-12-18T14%3A07%3A40.348Z',
-    author: 'Vincent Chansard',
-    series: 'Jujutsu Kaisen',
-    source: 'Season 2: Episode #17'
+    id: 7,
+    created_at: '2023-12-18T20:00:39.038967+00:00',
+    data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/masami_mori_weareone.gif?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL21hc2FtaV9tb3JpX3dlYXJlb25lLmdpZiIsImlhdCI6MTcwMjkyOTY0NSwiZXhwIjoxNzA1NTIxNjQ1fQ.LD0iPGA2uJQ0MmNpaYqnZFRMGco7MpGhUPYSt9aJgk8&t=2023-12-18T19%3A58%3A56.623Z',
+    author: 'Masami Mori',
+    series: 'One Piece',
+    source: 'WE ARE ONE (Special)'
+  },
+  {
+    id: 8,
+    created_at: '2023-12-18T20:01:30.904154+00:00',
+    data: 'https://ireumvmoybitxftsqlgh.supabase.co/storage/v1/object/sign/Cuts/bahijd_onepiece1015.gif?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJDdXRzL2JhaGlqZF9vbmVwaWVjZTEwMTUuZ2lmIiwiaWF0IjoxNzAyOTI5NjY3LCJleHAiOjE3MDU1MjE2Njd9.pqfxXwxhBrc5TVGf-s1Ig6F3NJfpT5tonGBnQAJgYPk&t=2023-12-18T19%3A59%3A17.973Z',
+    author: 'Bahi JD',
+    series: 'One piece',
+    source: 'Episode 1015'
   }
 ]
-
 module.exports = data
--- src/testing.js
+++ src/testing.js
@@ -1,1 +1,15 @@
-fetch('https://random-ka-v2.onrender.com').then(response => response.json()).then(data => console.log(data))
+const { createClient } = require('@supabase/supabase-js')
+
+const fetchData = async () => {
+  const supabase = createClient('https://ireumvmoybitxftsqlgh.supabase.co/', process.env.SUPABASE_KEY)
+  const { data, error } = await supabase
+    .from('sakuga')
+    .select('*')
+  return data
+}
+
+const newData = fetchData().then((data) => {
+  console.log(data)
+})
+
+module.exports = newData
